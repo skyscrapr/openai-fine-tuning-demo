0f7f406cf43f714a350df0d93f254beda28e1c1a
Richard Weerasinghe <richard.weerasinghe@youtap.com>
2023-09-05 20:12:48 +1200

    move from fine-tunes to fine-tuning_jobs

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,15 +22,13 @@ resource "openai_file" "validation_file" {
   filepath = "sport2_prepared_valid.jsonl"
 }
 
-resource "openai_finetune" "example" {
+resource "openai_finetuning_job" "example" {
   training_file                  = openai_file.training_file.id
   validation_file                = openai_file.validation_file.id
   model                          = "ada"
-  compute_classification_metrics = true
-  classification_positive_class  = " baseball"
   wait                           = true
 }
 
-output finetune_model {
-  value = openai_finetune.example.fine_tune.fine_tuned_model
+output fine_tuned_model {
+  value = openai_finetuning_job.example.fine_tuned_model
 }

f1b82ccec658f82d641c4fd2a6e1f5e126211f4f
Richard Weerasinghe <richard@skyscrapr.io>
2025-02-21 13:17:26 +1300

    upgrade example to new cookbook

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,50 @@
-import openai
+from openai import OpenAI
+from pprint import pprint
 
-ft_model = "ada:ft-personal-2023-08-24-02-29-58"
+import os
+import pandas as pd
 
-sample_hockey_tweet = """Thank you to the 
-@Canes
- and all you amazing Caniacs that have been so supportive! You guys are some of the best fans in the NHL without a doubt! Really excited to start this new chapter in my career with the 
-@DetroitRedWings
- !!"""
-res = openai.Completion.create(model=ft_model, prompt=sample_hockey_tweet + '\n\n###\n\n', max_tokens=1, temperature=0, logprobs=2)
-print("response: " + res['choices'][0]['text'])
+
+client = OpenAI()
+
+fine_tuned_model_id = "ft:gpt-4o-mini-2024-07-18:skyscrapr::B39ggHUD"
+
+system_message = "You are a helpful recipe assistant. You are to extract the generic ingredients from each of the recipes provided."
+
+
+def create_user_message(row):
+    return f"Title: {row['title']}\n\nIngredients: {row['ingredients']}\n\nGeneric ingredients: "
+
+
+def prepare_example_conversation(row):
+    return {
+        "messages": [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": create_user_message(row)},
+            {"role": "assistant", "content": row["NER"]},
+        ]
+    }
+
+
+recipe_df = pd.read_csv("data/cookbook_recipes_nlg_10k.csv")
+
+pprint(prepare_example_conversation(recipe_df.iloc[0]))
+
+recipe_df.head()
+
+test_df = recipe_df.loc[201:300]
+test_row = test_df.iloc[0]
+test_messages = []
+test_messages.append({"role": "system", "content": system_message})
+user_message = create_user_message(test_row)
+test_messages.append({"role": "user", "content": user_message})
+
+pprint(test_messages)
+
+
+
+
+response = client.chat.completions.create(
+    model=fine_tuned_model_id, messages=test_messages, temperature=0, max_tokens=500
+)
+print(response.choices[0].message.content)
